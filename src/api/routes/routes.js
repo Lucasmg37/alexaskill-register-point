a1c043a193067f0ce94421f5cf2fd12e53f4d129
Lucas Junior Dias de Paula <lucasmg37@live.com>
2021-03-26 23:02:13 -0300

    add alexa route

--- src/api/routes/routes.js
+++ src/api/routes/routes.js
@@ -4,5 +4,6 @@ const registerController = require('../controllers/register')
 
 const routes = express.Router()
 routes.get('/register', registerController.index)
+routes.post('/alexa', registerController.index)
 
 module.exports = routes
