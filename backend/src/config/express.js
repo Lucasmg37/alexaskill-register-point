1526fcc32ae66f91e2079a2bcc1b6a026a35182e
Lucas Junior Dias de Paula <lucasmg37@live.com>
2021-04-03 17:55:00 -0300

    add https

--- backend/src/config/express.js
+++ backend/src/config/express.js
@@ -1,17 +1,36 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
+const https = require('https')
+const http = require('http')
+
+const fs = require('fs')
+
 const routes = require('../api/routes/routes.js')
 
 module.exports = () => {
   const app = express()
 
-  app.set('port', process.env.PORT || 3333)
-
   app.use(bodyParser.json())
   app.use(cors())
   app.use('/assets', express.static('./assets'))
   app.use(routes)
+
+  // Listen both http & https ports
+  const httpServer = http.createServer(app)
+  const httpsServer = https.createServer({
+    key: fs.readFileSync('./config/web_server.csr'),
+    cert: fs.readFileSync('./config/web_server_fake_PEM.key')
+  }, app)
+
+  httpServer.listen(3334, () => {
+    console.log('HTTP Server running on port 3334')
+  })
+
+  httpsServer.listen(3333, () => {
+    console.log('HTTPS Server running on port 3333')
+  })
 
   return app
 }
